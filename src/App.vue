ee3befbff3631673c96af41a4b088a231c9da6d9
Alberts Zemzale <alberts.zemzale@gmail.com>
2020-08-09 14:52:04 +0300

    Remove default vue stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+    --color-bg-main: #0b0c10;
+    --color-bg-secondary: #1f2833;
+    --color-text: #c5c6c7;
+    --color-accent-main: #66fcf1;
+    --color-accent-secondary: #45a29e;
+}
+
+html, body {
+    height: 100%;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+    max-width: 700px;
+    margin:0 auto;
+    font-family: "Space Mono", monospace;
+    font-size: 26px;
+    background-color: var(--color-bg-main);
+    color: var(--color-text);
+}
+
+.content {
+    flex: 1 0 auto;
+}
+
+.footer {
+    flex-shrink: 0;
+    border-top: 3px solid var(--color-accent-secondary);
+}
+
+h1,
+h2,
+h3 {
+    line-height:1.2
 }
 </style>
