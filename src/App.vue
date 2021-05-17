3120344cc45df3df671e64124407a37a63542584
Alberts Zemzale <alberts.zemzale@testdevlab.com>
2021-05-17 09:46:58 +0300

    Create the start to the page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="holder">
+        <div class="card">
+            <div class="card-header">
+                <span>Fun</span>
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li><a href="https://reddit.com/r/programming">/r/programming</a></li>
+                    <li><a href="https://youtube.com">YouTube</a></li>
+                    <li><a href="https://netflix.com">Netflix</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <span>Code</span>
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li><a href="https://github.com">GitHub</a></li>
+                    <li><a href="https://gitlab.com">GitLab</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+.holder {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.card-body {
+    margin: 5px 5px 0px 5px;
+}
+
+.card {
+    display: inline;
+    margin: 10px;
+}
+
+.card-header {
+    border-bottom: #11e6be solid 1px;
+    padding-bottom: 2px;
+}
+body {
+    background-color: #121212;
+}
+a {
+    color: #f1f1f1;
+    text-decoration: none;
+}
+a:hover {
+    color: #121212;
+}
+
+li:hover {
+    color: #121212;
+    background-color: #11e6be;
+    text-decoration: none;
+}
+ul {
+    padding:0;
+    margin:0;
+    text-align: left;
+    list-style-type: none;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Roboto Mono, monospace;
   text-align: center;
-  color: #2c3e50;
+  color: #f1f1f1;
   margin-top: 60px;
 }
 </style>
