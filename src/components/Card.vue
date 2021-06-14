cb9e7fc682ff29fb74f5eaffc4c31bbf2deddb10
Alberts Zemzale <alberts.zemzale@testdevlab.com>
2021-06-14 10:42:33 +0300

    Move card into a component

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <span>{{ title }}</span>
+        </div>
+        <div class="card-body">
+            <ul>
+                <li v-for="item in items" :key="item.link"><a :href="item.link">{{ item.name }}</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: String,
+    items: Array,
+  }
+}
+</script>
