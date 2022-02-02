4a20cf15d5a08d91ef724d9435e04dd7ede4ba22
Alberts Zemzale <alberts.zemzale@gmail.com>
2022-02-02 21:00:19 +0200

    feat: Add ability to add more categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import './App.css'
+import React, { useState } from 'react'
 import { Category, DefaultCategories } from './models/Category'
 import { Card } from './components/Card'
 
 function App() {
+  const [categories, setCategories] = useState(DefaultCategories)
+  const [t, setT] = useState("value")
+
+  const submitForm = (event: React.FormEvent) => {
+    event.preventDefault()
+    const name = t
+    //TODO @zemzale 02/02/22 Figure out how to add the fucking links to the cateory
+    setCategories(categories => [...categories, 
+      {
+        name: name, 
+        links: []
+      }
+    ]
+    )
+  }
+   
   return (
     <div className="App">
         <div className="holder"> 
             {
-                DefaultCategories.map((category: Category, i: number) => 
+                categories.map((category: Category, i: number) => 
                     <Card key={ i } name={ category.name } links={ category.links } />
                 )
             }
         </div>
+        <form onSubmit={submitForm}>
+          <input type="text" value={t} onChange={(e) => {
+            setT(e.target.value)
+          }}/>
+          <input type="submit" value="submit"/>
+        </form>
     </div>
   )
 }
